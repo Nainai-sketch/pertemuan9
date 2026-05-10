4a50859cd0472c2678db7f42391036241e2d9a4e
unknown <ladayyafaroh@gmail.com>
2026-05-10 22:50:00 +0700

    menambahkan kolom status

--- data_cars.R
+++ data_cars.R
@@ -1,2 +1,11 @@
 summary(mtcars)
 ratarata<-mean(mtcars$mpg)
+n<-length(mtcars)
+status=c()
+for (i in 1:n){
+  if (mtcars$mpg[i]>ratarata){
+    status<-append(status,'tinggi')
+  }
+  else
+    status<-append(status,'rendah')
+}
